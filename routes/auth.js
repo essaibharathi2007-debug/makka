b685fbb19d80e0a473300ea90c820a37233b3ab5
essaibharathi2007-debug <essaibharathi2007@gmail.com>
2026-07-01 10:52:36 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,52 +1,46 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const router = express.Router();
-const mongoose = require('mongoose');
-
-const User = mongoose.model('User', {
-  name: String,
- phone: { type: String, unique: true },
-  username: String,
-  password: String
-});
+const User = require('../models/User');
 
 router.post('/register', async (req, res) => {
   const { name, phone, username, password } = req.body;
 
   const exist = await User.findOne({ phone });
-if (exist) {
-  return res.json({
-    success: false,
-    message: "Phone already exists"
-  });
-}
-  try {
-  const hashedPassword = await bcrypt.hash(password, 10);
-
-const user = new User({
-  name,
-  phone,
-  username,
-  password: hashedPassword
-});
-
-await user.save();
-  res.json({ success: true });
-
-} catch (err) {
-
-  if (err.code === 11000) {
-    return res.status(400).json({
+  if (exist) {
+    return res.json({
       success: false,
-      message: "Phone number already registered"
+      message: "Phone already exists"
     });
   }
 
-  return res.status(500).json({
-    success: false,
-    message: "Server error"
-  });
-}
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = new User({
+      name,
+      phone,
+      username,
+      password: hashedPassword
+    });
+
+    await user.save();
+    res.json({ success: true });
+
+  } catch (err) {
+
+    if (err.code === 11000) {
+      return res.status(400).json({
+        success: false,
+        message: "Phone number already registered"
+      });
+    }
+
+    return res.status(500).json({
+      success: false,
+      message: "Server error"
+    });
+  }
 });
 
 router.post('/login', async (req, res) => {
@@ -59,6 +53,9 @@ router.post('/login', async (req, res) => {
   const isMatch = await bcrypt.compare(password, user.password);
 
   if (!isMatch) return res.json({ success: false });
+
+  user.lastLogin = new Date();
+  await user.save();
 
   res.json({ success: true, user });
 });
